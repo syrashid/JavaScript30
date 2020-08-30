cfc873dac6ec305ae4158caaf7c859213f8de049
Sy Rashid <sy.rashid.11@gmail.com>
2020-08-30 22:57:03 +1000

    Dropped in getVideo function, made it async

--- 19 - Webcam Fun/scripts.js
+++ 19 - Webcam Fun/scripts.js
@@ -3,3 +3,16 @@ const canvas = document.querySelector('.photo');
 const ctx = canvas.getContext('2d');
 const strip = document.querySelector('.strip');
 const snap = document.querySelector('.snap');
+
+async function getVideo() {
+  try {
+    const localMediaStream = await navigator.mediaDevices.getUserMedia({ video: true, audio: false });
+    console.log(localMediaStream);
+    video.srcObject = localMediaStream;
+    video.play();
+  } catch (err) {
+      console.error(`OH NO!!!`, err);
+    };
+}
+
+getVideo();
