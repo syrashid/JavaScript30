13a7a7fabde2fe097b57b7c98de4e4f72238bf69
Sy Rashid <sy.rashid.11@gmail.com>
2020-09-08 22:17:19 +1000

    Added basic timer

---
+++ 29 - Countdown Timer/scripts-START.js
@@ -0,0 +1,40 @@
+let countdown;
+const timerDisplay = document.querySelector('.display__time-left');
+const endTime = document.querySelector('.display__end-time');
+const buttons = document.querySelectorAll('[data-time]');
+
+
+function timer(seconds) {
+  const now = Date.now();
+  const then = now + seconds * 1000;
+
+  displayTimeLeft(Math.round((then - now)/1000));
+  displayEndTime(then);
+
+  countdown = setInterval(() => {
+    const secondsLeft = Math.round((then - Date.now()) / 1000);
+    if (secondsLeft < 0) {
+      clearInterval(countdown);
+      return;
+    }
+
+  displayTimeLeft(secondsLeft);
+  }, 1000);
+}
+
+function displayTimeLeft(seconds) {
+  const minutes = Math.floor(seconds / 60);
+  const remainderSeconds = seconds % 60;
+  const display = `${minutes}:${remainderSeconds < 10 ? '0' : ''}${remainderSeconds}`;
+
+  document.title = display;
+  timerDisplay.textContent = display;
+}
+
+function displayEndTime(timestamp) {
+  const end = new Date(timestamp);
+  const hour = end.getHours();
+  const minutes = end.getMinutes();
+  endTime.textContent =`Be back at ${hour > 12 ? hour - 12 : hour === 0 ? 12 : hour}:${minutes < 10 ? '0': ''}${minutes}`;
+
+}
